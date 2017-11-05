e36297268eda257eba507f95644b2185ce82d9db
xt57 <xt57dev@gmail.com>
2017-11-04 22:10:39 -0500

    committing the contents of the new LogTools.cs

--- LogTools.cs
+++ LogTools.cs
@@ -1,10 +1,42 @@
-﻿using System;
-namespace xmlDev97
-{
-    public class EmptyClass
-    {
-        public EmptyClass()
-        {
-        }
-    }
+﻿
+//
+//
+//
+
+using System;
+
+
+namespace XmlDemo {
+
+
+public
+class
+LogTools {
+
+    bool            bStatus;
+
+
+public
+LogTools () {
+
+    bStatus = true;
+
+    //  ================
 }
+
+ 
+public
+bool
+Log ( String sInMsg ) {
+
+    System.Console.WriteLine( sInMsg );
+    return true;
+}
+
+
+
+
+}   //  end of xmlTools class
+
+
+}   //  end of xmlTools namespace
